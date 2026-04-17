99235482345ce02a52dcdd72d8812dbafbb2f641
Balthazar Condemine <you@example.com>
2026-04-17 15:25:27 +0200

    connections almost done

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,12 +1,21 @@
 import os
+from enum import Enum
+
+
+class Utils(Enum):
+    START_HUB = "start_hub"
+    END_HUB = "end_hub"
+    HUB = "hub"
+    CONNECTION = "connection"
+    # NB_DRONES = to do later
 
 
 class Hubs:
     def __init__(
                  self,
                  name: str,
-                 x: int,
-                 y: int,
+                 x: str,
+                 y: str,
                 #  color: str,
                 #  zone_type: str
                  ):
@@ -17,7 +26,7 @@ class Hubs:
         # to add colors, zone type by default and list of connections
 
 
-def get_maps() -> list | None:
+def get_maps():
     output = []
     for x in os.listdir("maps"):
         if x.endswith(".txt"):
@@ -30,8 +39,9 @@ def get_maps() -> list | None:
 def map_valid(my_map):
     with open("maps/" + my_map) as f:
         my_hubs = {}
-        my_hubs["start_hub"] = {}
-        my_hubs["end_hub"] = {}
+        my_hubs[Utils.START_HUB] = {}
+        my_hubs[Utils.END_HUB] = {}
+        hub_links = []
         format_error = "Format Error: Must respect pattern 'nb_drones: int'"
         i = 0
         j = 1
@@ -42,7 +52,9 @@ def map_valid(my_map):
                 raise ValueError(format_error + f"Line: {j}")
             else:
                 if i == 0 and "nb_drones" not in line:
-                    raise ValueError(f"First line must be 'nb_drones', Line: {j}")
+                    raise ValueError(
+                          f"First line must be 'nb_drones', Line: {j}"
+                          )
                 elif i == 0:
                     temp = line.split(":")
                     int(temp[1])
@@ -53,23 +65,35 @@ def map_valid(my_map):
                 else:
                     key = line.split(":")
                     data = key[1].split()
-                    if key[0] == "start_hub":
-                        if my_hubs["start_hub"] != {}:
+                    if key[0] == Utils.START_HUB:
+                        if my_hubs[Utils.START_HUB] != {}:
                             raise ValueError("Can't init twice start,"
                                              f"Line: {j}")
-                        my_hubs["start_hub"] = Hubs(data[0], data[1], data[2]) #a voir pour les couleurs 
-                    elif key[0] == "end_hub":
-                        if my_hubs["end_hub"] != {}:
-                            raise ValueError(f"Cannot init twice end_hub, Line: {j}")
-                        my_hubs["end_hub"] = Hubs(data[0], data[1], data[2])
-                    elif key[0] == "hub":
+                        my_hubs[Utils.START_HUB] = Hubs(
+                                                   data[0], data[1], data[2]
+                                                   )  #a voir pour les couleurs
+                    elif key[0] == Utils.END_HUB:
+                        if my_hubs[Utils.END_HUB] != {}:
+                            raise ValueError(
+                                  f"Cannot init twice end_hub, Line: {j}"
+                                  )
+                        my_hubs[Utils.END_HUB] = Hubs(
+                                                 data[0], data[1], data[2]
+                                                 )
+                    elif key[0] == Utils.HUB:
                         my_hubs[data[0]] = Hubs(data[0], data[1], data[2])
+                    elif key[0] == Utils.CONNECTION:
+                        hub_links.append(key[1].strip()) # do later ft_process connections that links eery hubs
                     else:
-                        raise ValueError(f"Unknown Type, {format_error}, Line: {j}")
-
-# to do later the connections stuff to handle
+                        raise ValueError(
+                              f"Unknown Type, {format_error}, Line: {j}"
+                              )
                 i = 1
             j += 1
+    print(hub_links)
+
+# to do later the connections stuff to handle
+
     for elt in my_hubs.values():
         try:
             print(elt.name)
@@ -80,7 +104,7 @@ def map_valid(my_map):
 def main():
     try:
         maps = get_maps()
-        if len(maps) == 0:
+        if len(maps) == 0: #or not maps
             raise ValueError("At least one map must be available")
     except ValueError as e:
         print(f"File error: {e}")
