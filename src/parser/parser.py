cd77b22f4baca239d75a076c08db93c4a4d7e2f8
Balthazar Condemine <you@example.com>
2026-04-20 16:48:26 +0200

    improved parsing handled some new errors

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -10,21 +10,41 @@ class Utils(StrEnum):
     # NB_DRONES = to do later
 
 
+class ZoneType(StrEnum):
+    NORMAL = "normal"
+    BLOCKED = "blocked"
+    RESTRICTED = "restricted"
+    PRIORITY = "priority"
+
+
 class Hubs:
     def __init__(
                  self,
                  name: str,
                  x: str,
                  y: str,
-                 check: tuple
+                 check: tuple,
+                 zone_type=ZoneType.NORMAL,
+                 max_capacity=1
                 #  color: str,
-                #  zone_type: str
                  ):
         self.name = name
         self.x = int(x)
         self.y = int(y)
         self.links = []
         self.start, self.end = check
+        self.max_flow = int(max_capacity)
+        if zone_type == ZoneType.NORMAL:
+            self.cost = float(1)
+        elif zone_type == ZoneType.BLOCKED:
+            self.cost = float('inf')
+        elif zone_type == ZoneType.RESTRICTED:
+            self.cost = 2
+        elif zone_type == ZoneType.PRIORITY:
+            self.cost = float(0.9)
+        else:
+            raise ValueError(f"Wrong zone type: {zone_type} does not exist")
+
         # to add some chekc for zone wtype and colors and list of connections
         # to add colors, zone type by default and list of connections
 
@@ -42,8 +62,8 @@ def get_maps():
 def map_valid(my_map):
     with open("maps/" + my_map) as f:
         my_hubs = {}
-        my_hubs[Utils.START_HUB] = {}
-        my_hubs[Utils.END_HUB] = {}
+        start_name = ""
+        end_name = ""
         my_hubs["hubs_links"] = []
         format_error = "Format Error: Must respect pattern 'nb_drones: int'"
         i = 0
@@ -59,8 +79,13 @@ def map_valid(my_map):
                           f"First line must be 'nb_drones', Line: {j}"
                           )
                 elif i == 0:
-                    temp = line.split(":")
-                    int(temp[1])
+                    try:
+                        temp = line.split(":")
+                        int(temp[1])
+                        if len(temp) != 2:
+                            raise ValueError
+                    except Exception:
+                        raise ValueError(f"{format_error}, Line: {j}")
                     if temp[0] != "nb_drones":
                         raise ValueError(format_error + f"Line: {j}")
                     else:
@@ -69,15 +94,10 @@ def map_valid(my_map):
                     key = line.split(":")
                     data = key[1].split()
                     if key[0] == Utils.START_HUB:
-                        if my_hubs[Utils.START_HUB] != {}:
+                        if start_name != "":
                             raise ValueError("Can't init twice start,"
-                                             f"Line: {j}")
-                        my_hubs[Utils.START_HUB] = Hubs(
-                                                   Utils.START_HUB,
-                                                   data[1],
-                                                   data[2],
-                                                   (True, False)
-                                                   )  #a voir pour les couleurs
+                                             f"Line: {j}") #a voir pour les couleurs
+                        start_name = data[0]
                         my_hubs[data[0]] = Hubs(
                                                 data[0],
                                                 data[1],
@@ -85,15 +105,11 @@ def map_valid(my_map):
                                                 (True, False)
                                                 )
                     elif key[0] == Utils.END_HUB:
-                        if my_hubs[Utils.END_HUB] != {}:
+                        if end_name != "":
                             raise ValueError(
                                   f"Cannot init twice end_hub, Line: {j}"
                                   )
-                        my_hubs[Utils.END_HUB] = Hubs(
-                                                 Utils.END_HUB,
-                                                 data[1], data[2],
-                                                 (False, True)
-                                                 )
+                        end_name = data[0]
                         my_hubs[data[0]] = Hubs(
                                                 data[0], data[1], data[2],
                                                 (False, True)
@@ -104,18 +120,18 @@ def map_valid(my_map):
                                                 data[2],
                                                 (False, False))
                     elif key[0] == Utils.CONNECTION:
-                        my_hubs["hubs_links"].append(key[1].strip()) # do later ft_process connections that links eery hubs
+                        my_hubs["hubs_links"].append(key[1].strip())
                     else:
                         raise ValueError(
                               f"Unknown Type, {format_error}, Line: {j}"
                               )
                 i = 1
             j += 1
-    if my_hubs[Utils.START_HUB] == {}:
+    if start_name == "":
         raise ValueError(
                          f"MISSING : {Utils.START_HUB}"
                          )
-    if my_hubs[Utils.END_HUB] == {}:
+    if end_name == "":
         raise ValueError(
                          f"MISSING : {Utils.END_HUB}"
                          )
@@ -171,29 +187,25 @@ def main():
         maps = get_maps()
         if len(maps) == 0 or not maps:
             raise ValueError("At least one map must be available")
-    except ValueError as e:
+    except Exception as e:
         print(f"File error: {e}")
         return
-    except Exception as e:
-        print(f"Caught Error : {e}")
-        return
-    try:
-        for elt in maps:
-            full_maps[elt] = map_valid(elt)
-    except ValueError as e:
-        print(e)
-        return
-    except BaseException as e:
-        print(e)
-        return
-    choosen_map = "01_linear_path.txt" #to define after full_maps created
-    try:
-        displayable_map = make_links(full_maps[choosen_map])
-    except Exception as e:
-        print(e)
-        return
-    print(displayable_map["start"].links)
+    valid_maps = []
+    for elt in maps:
+        full_maps[elt] = map_valid(elt)
+        valid_maps.append(elt)
+    choosen_map = "02_simple_fork.txt"  #to define after full_maps created
+    if choosen_map not in valid_maps:
+        raise ValueError("The choosen map is not valid")
+    displayable_map = make_links(full_maps[choosen_map])
+    for elt in displayable_map.values():
+        if isinstance(elt, Hubs):
+            print(elt.name, elt.links)
+            print()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except BaseException as e:
+        print(e)
 # to add try except for main at the end
